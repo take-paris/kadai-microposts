8732f0006e7605d3ec41481c3c0dcd040242ba5c
jiro.kirameki <ec2-user@ip-172-31-37-215.us-east-2.compute.internal>
2018-06-20 06:32:41 +0000

    timeline

--- app/User.php
+++ app/User.php
@@ -41,4 +41,11 @@ class User extends Authenticatable
     {
         return $this->belongsToMany(User::class, 'user_follow', 'follow_id', 'user_id')->withTimestamps();
     }
+    
+    public function feed_microposts()
+    {
+        $follow_user_ids = $this->followings()-> pluck('users.id')->toArray();
+        $follow_user_ids[] = $this->id;
+        return Micropost::whereIn('user_id', $follow_user_ids);
+    }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,15 @@
 @section('content')
     @if (Auth::check())
         <div class="row">
-            <aside class="col-md-4">
+            <aside class="col-xs-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">{{ $user->name }}</h3>
+                    </div>
+                    <div class="panel-body">
+                        <img class="media-object img-rounded img-responsive" src="{{ Gravatar::src($user->email, 500) }}" alt="">
+                    </div>
+                </div>
             </aside>
             <div class="col-xs-8">
                 @if (count($microposts) > 0)
